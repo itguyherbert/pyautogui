f508eaa1877ffce03f8ecd6a72e4ea27bf300f78
itguyherbert <70749412+itguyherbert@users.noreply.github.com>
2020-09-04 11:08:23 +0200

    Add files via upload

--- pyautogui-hearthstone.py
+++ pyautogui-hearthstone.py
@@ -1,5 +1,9 @@
+# Uncomment "import time" and "time.sleep" if you want the script to wait before running.
+# This way, you can also run the script on one monitor, when you can't move vscode over to a second monitor.
+#import time
 import pyautogui
 cards = int(input("How many cards do you want to disenchant?"))
+#time.sleep(5)
 for x in range(cards):
     pyautogui.moveTo(426, 296, 0.3)
     pyautogui.click(button='right')
